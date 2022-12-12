c2421ace3e89ce485f904c8cff7e736e353c6847
Docky44 <simonpierre643@gmail.com>
2022-12-12 09:14:20 +0100

    final commit

--- src/Form/ChatonSupprimerType.php
+++ src/Form/ChatonSupprimerType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ChatonsSupprimerType extends AbstractType
+class ChatonSupprimerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
